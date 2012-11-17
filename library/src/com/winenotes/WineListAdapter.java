0a6cecdbeb16da28f7172fef4cf8c45a19896086
Janos Gyerik <janos.gyerik@gmail.com>
2012-11-17 21:00:10 +0100

    added wine type icons in list

--- library/src/com/winenotes/WineListAdapter.java
+++ library/src/com/winenotes/WineListAdapter.java
@@ -6,21 +6,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class WineListAdapter extends CursorAdapter {
 
-	private LayoutInflater mLayoutInflater;
-	private int mIdIndex;
-	private int mNameIndex;
-	private int mSummaryIndex;
-	private int mRatingIndex;
+	private static final int RED_WINE = 1;
+	private static final int WHITE_WINE = 2;
+
+	private final LayoutInflater mLayoutInflater;
+	private final int mIdIndex;
+	private final int mWineTypeIdIndex;
+	private final int mNameIndex;
+	private final int mSummaryIndex;
+	private final int mRatingIndex;
 
 	public WineListAdapter(Context context, Cursor cursor) {
 		super(context, cursor, true);
 
 		mIdIndex = cursor.getColumnIndex("_id");
+		mWineTypeIdIndex = cursor.getColumnIndex("winetype_id");
 		mNameIndex = cursor.getColumnIndex("name");
 		mSummaryIndex = cursor.getColumnIndex("summary");
 		mRatingIndex = cursor.getColumnIndex("rating");
@@ -30,10 +36,24 @@ public class WineListAdapter extends CursorAdapter {
 
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
-		TextView _ID = (TextView)view.findViewById(R.id._ID);
+		TextView _ID = (TextView) view.findViewById(R.id._ID);
 		_ID.setText(cursor.getString(mIdIndex));
 
-		TextView name = (TextView)view.findViewById(R.id.name);
+		int imageResourceId;
+		switch (cursor.getInt(mWineTypeIdIndex)) {
+		case RED_WINE:
+			imageResourceId = R.drawable.bullet_redwine;
+			break;
+		case WHITE_WINE:
+			imageResourceId = R.drawable.bullet_whitewine;
+			break;
+		default:
+			imageResourceId = R.drawable.bullet_other;
+		}
+		ImageView bullet = (ImageView) view.findViewById(R.id.bullet);
+		bullet.setImageResource(imageResourceId);
+
+		TextView name = (TextView) view.findViewById(R.id.name);
 		name.setText(cursor.getString(mNameIndex));
 
 		TextView summary = (TextView) view.findViewById(R.id.summary);
--- library/src/com/winenotes/WineNotesSQLiteOpenHelper.java
+++ library/src/com/winenotes/WineNotesSQLiteOpenHelper.java
@@ -613,7 +613,8 @@ public class WineNotesSQLiteOpenHelper extends SQLiteOpenHelper {
 		Log.d(TAG, "get all wines");
 		Cursor cursor = getReadableDatabase().rawQuery(String.format(
 				"SELECT w._id, ifnull(nullif(w.name, ''), '(noname)') name, " +
-						"listing_text summary, overall_rating rating " +
+						"listing_text summary, overall_rating rating, " +
+						"winetype_id " +
 						"FROM %s w " +
 						"LEFT JOIN %s f ON w.flag_id = f._id " +
 						"ORDER BY ifnull(f.display_order, 100), overall_rating DESC, w.updated_dt DESC",
